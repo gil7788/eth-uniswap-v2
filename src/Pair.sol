28b6fc3d065aa94d224820f2aa721201db1c6b10
gil7788 <gil7788@gmail.com>
2024-11-03 16:53:18 +0200

    Implement Pair
    - Tests
    - Add RetrancyGuarding against malicious tokens

--- src/Pair.sol
+++ src/Pair.sol
@@ -1,4 +1,130 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-contract Pair {}
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "src/LPToken.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+contract Pair is Ownable, ReentrancyGuard {
+    LPToken _lpToken;
+    ERC20 _token0;
+    ERC20 _token1;
+    string _name;
+
+    constructor(
+        string memory tokenName,
+        string memory tokenSymbol,
+        ERC20 token0,
+        ERC20 token1
+    ) Ownable(msg.sender) {
+        _lpToken = new LPToken(tokenName, tokenSymbol, 0);
+        _token0 = token0;
+        _token1 = token1;
+        string memory token0Symbol = token0.symbol();
+        string memory token1Symbol = token1.symbol();
+        _name = string(
+            abi.encodePacked("uniswap-", token0Symbol, "/", token1Symbol)
+        );
+    }
+
+    function provideInitLiqudity(
+        uint256 token1Amount,
+        uint256 token2Amount
+    ) public onlyOwner nonReentrant {
+        _addLiquidity(token1Amount, token2Amount);
+    }
+
+    // Swap functionality
+    function swap(
+        address from,
+        address to,
+        uint256 amount
+    ) public nonReentrant {
+        address tokenAAddress = address(_token0);
+        address tokenBAddress = address(_token1);
+        address owner = _msgSender();
+        require(
+            (from == tokenAAddress && to == tokenBAddress) ||
+                (from == tokenBAddress && to == tokenAAddress),
+            "Invalid token pair"
+        );
+
+        ERC20 fromToken = ERC20(from);
+        ERC20 toToken = ERC20(to);
+        require(fromToken.balanceOf(owner) >= amount, "Insufficient Amount");
+
+        uint256 price = getTokenPrice(from, to, amount);
+        fromToken.transferFrom(owner, address(this), amount);
+        toToken.transfer(owner, price);
+    }
+
+    function getTokenPrice(
+        address from,
+        address to,
+        uint256 amount
+    ) internal view returns (uint256) {
+        ERC20 erc20From = ERC20(from);
+        ERC20 erc20To = ERC20(to);
+        uint256 balanceFrom = erc20From.balanceOf(address(this));
+        uint256 balanceTo = erc20To.balanceOf(address(this));
+
+        uint256 price = (ceilDiv(balanceFrom, balanceFrom + amount) - 1) *
+            balanceTo;
+        return price;
+    }
+
+    function addLiquidity(address token, uint256 amount) public nonReentrant {
+        require(
+            token == address(_token0) || token == address(_token1),
+            "Invalid token"
+        );
+
+        ERC20 erc20Token = ERC20(token);
+        uint256 balanceFrom = erc20Token.balanceOf(address(this));
+
+        if (token == address(_token0)) {
+            uint256 balanceTo = _token1.balanceOf(address(this));
+            uint256 amountTo = ceilDiv(amount * balanceTo, balanceFrom);
+            _addLiquidity(amount, amountTo);
+        } else if (token == address(_token1)) {
+            uint256 balanceTo = _token0.balanceOf(address(this));
+            uint256 amountTo = ceilDiv(amount * balanceTo, balanceFrom);
+            _addLiquidity(amount, amountTo);
+        }
+    }
+
+    function _addLiquidity(uint256 amountA, uint256 amountB) internal {
+        address owner = _msgSender();
+        approve(address(_token0), amountA);
+        _token0.transferFrom(owner, address(this), amountA);
+        approve(address(_token1), amountB);
+        _token1.transferFrom(owner, address(this), amountB);
+    }
+
+    function approve(address token, uint256 amount) public {
+        require(
+            token == address(_token0) || token == address(_token1),
+            "Invalid Token"
+        );
+        ERC20 erc20Token = ERC20(token);
+        erc20Token.approve(address(this), amount);
+    }
+
+    function ceilDiv(uint256 x, uint256 y) public pure returns (uint256) {
+        require(y != 0, "Invalid division by 0");
+        if (x == 0) {
+            return 0;
+        }
+        uint256 result = 1 + (x - 1) / y;
+        return result;
+    }
+
+    function name() external view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() external view returns (string memory) {
+        return _lpToken.symbol();
+    }
+}
--- test/Pair.t.sol
+++ test/Pair.t.sol
@@ -1,23 +1,130 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
+import "../src/Pair.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-import "src/Pair.sol";
+contract MockERC20 is ERC20 {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialAmount
+    ) ERC20(name, symbol) {
+        _mint(msg.sender, initialAmount);
+    }
+}
 
 contract TestPair is Test {
-    Pair c;
+    Pair private pair;
+    MockERC20 token1;
+    MockERC20 token2;
+    address private owner;
+    uint256 initialLiquidity;
+    uint8 decimals = 18;
 
     function setUp() public {
-        c = new Pair();
+        owner = address(this);
+        uint256 token1Amount = 1000 * 10 ** decimals;
+        uint256 token2Amount = 1000 * 10 ** decimals;
+        token1 = new MockERC20("Token1", "TK1", token1Amount);
+        token2 = new MockERC20("Token2", "TK2", token2Amount);
+        pair = new Pair("TEST-LPT", "TLPT", token1, token2);
+
+        // Approve Pair contract to transfer tokens on behalf of the owner
+        token1.approve(address(pair), type(uint256).max);
+        token2.approve(address(pair), type(uint256).max);
+
+        // Transfer initial liquidity to the pair contract
+        token1.transfer(address(pair), 500 * 10 ** decimals);
+        token2.transfer(address(pair), 500 * 10 ** decimals);
+        initialLiquidity =
+            token1.balanceOf(address(pair)) *
+            token2.balanceOf(address(pair));
     }
 
-    function testBar() public {
-        assertEq(uint256(1), uint256(1), "ok");
+    function testProvideInitLiquidity() public {
+        uint256 token1Amount = 100 * 10 ** decimals;
+        uint256 token2Amount = 100 * 10 ** decimals;
+
+        // Provide initial liquidity
+        pair.provideInitLiqudity(token1Amount, token2Amount);
+
+        // Check pair contract's balance for token1 and token2
+        assertEq(token1.balanceOf(address(pair)), 600 * 10 ** decimals);
+        assertEq(token2.balanceOf(address(pair)), 600 * 10 ** decimals);
     }
 
-    function testFoo(uint256 x) public {
-        vm.assume(x < type(uint128).max);
-        assertEq(x + x, x * 2);
+    function testSwapToken1ToToken2() public {
+        uint256 amountToSwap = 10 * 10 ** decimals;
+
+        // Perform swap: token1 -> token2
+        pair.swap(address(token1), address(token2), amountToSwap);
+
+        // Check balances after swap
+        assertEq(token1.balanceOf(owner), 490 * 10 ** decimals); // 1000 - 10
+        assertTrue(token2.balanceOf(owner) > 0); // Received some token2
+        uint256 liquidity = token1.balanceOf(address(pair)) *
+            token2.balanceOf(address(pair));
+        assertGe(liquidity, initialLiquidity);
+    }
+
+    function testSwapToken2ToToken1() public {
+        uint256 amountToSwap = 10 * 10 ** decimals;
+
+        // Perform swap: token2 -> token1
+        pair.swap(address(token2), address(token1), amountToSwap);
+
+        // Check balances after swap
+        assertEq(token2.balanceOf(owner), 490 * 10 ** decimals); // 1000 - 10
+        assertTrue(token1.balanceOf(owner) > 0); // Received some token1
+        uint256 liquidity = token1.balanceOf(address(pair)) *
+            token2.balanceOf(address(pair));
+        assertGe(liquidity, initialLiquidity);
+    }
+
+    function testAddLiquidityToken1() public {
+        uint256 token1Amount = 50 * 10 ** decimals;
+        pair.addLiquidity(address(token1), token1Amount);
+        // Check pair contract's token1 balance
+        assertEq(token1.balanceOf(address(pair)), 550 * 10 ** decimals);
+    }
+
+    function testAddLiquidityToken2() public {
+        uint256 token2Amount = 50 * 10 ** decimals;
+        pair.addLiquidity(address(token2), token2Amount);
+        // Check pair contract's token2 balance
+        assertEq(token2.balanceOf(address(pair)), 550 * 10 ** decimals);
+    }
+
+    function testFailSwapInvalidTokenPair() public {
+        uint256 amountToSwap = 10 * 10 ** decimals;
+        pair.swap(address(0), address(token1), amountToSwap);
+    }
+
+    function testFailAddLiquidityZeroToken() public {
+        uint256 amountToAdd = 50 * 10 ** decimals;
+        pair.addLiquidity(address(0), amountToAdd);
+    }
+
+    /* ceilDiv */
+    function testCeilDivZeroNominator() public {
+        uint256 result = pair.ceilDiv(0, 100);
+        assertEq(result, 0);
+    }
+
+    function testCeilDivByZero() public {
+        vm.expectRevert("Invalid division by 0");
+        pair.ceilDiv(100, 0);
+    }
+
+    function testCeilDivOne() public {
+        uint256 result = pair.ceilDiv(1, 100);
+        assertTrue(result == 1);
+    }
+
+    function testCeilDiv() public {
+        uint256 result = pair.ceilDiv(455, 100);
+        assertTrue(result == 5);
     }
 }
